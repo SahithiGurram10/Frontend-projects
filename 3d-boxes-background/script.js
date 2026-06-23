052560c4e4ebaff0d3295c8fb5a442204de5ac10
SahithiGurram10 <112694611+SahithiGurram10@users.noreply.github.com>
2026-06-23 12:17:57 +0530

    3d boxes background

--- 3d-boxes-background/script.js
+++ 3d-boxes-background/script.js
@@ -4,7 +4,6 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
 
-
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
